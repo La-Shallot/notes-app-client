daff7c4da521cb4b89a2265ed4204883537ff027
Charlotte Wang <shallotwang129@gmail.com>
2019-12-27 16:36:01 -0500

    Finished React + Amplify Setup

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-api-prod-serverlessdeploymentbucket-4c2pmfkfukz"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://by1wq1rb65.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2"",
+    USER_POOL_ID: "us-east-2_TnaQmdurB",
+    APP_CLIENT_ID: "4m79tesmqbsmlbc2jir2sc72b8",
+    IDENTITY_POOL_ID: "us-east-2:381f21a1-ca7b-4ab5-93e6-2f20666be2b2"
+  }
+};
---
+++ src/containers/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./NotFound.css"
+
+export default function NotFound(){
+  return(
+    <div className = "NotFound">
+      <h1>Error 404</h1>
+      <h3> Sorry, page <b>not</b> found!</h3>
+      <p>LMAO I actually took the time to write this page. Ur all welcome.</p>
+    </div>
+  );
+}
