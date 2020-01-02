59230f883236b89040ee5544e15fffb4468e65b1
Charlotte Wang <shallotwang129@gmail.com>
2020-01-01 19:48:42 -0500

    done with frontend

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-2",
     BUCKET: "notes-app-api-prod-serverlessdeploymentbucket-4c2pmfkfukz"
@@ -8,7 +9,7 @@ export default {
     URL: "https://by1wq1rb65.execute-api.us-east-2.amazonaws.com/prod"
   },
   cognito: {
-    REGION: "us-east-2"",
+    REGION: "us-east-2",
     USER_POOL_ID: "us-east-2_TnaQmdurB",
     APP_CLIENT_ID: "4m79tesmqbsmlbc2jir2sc72b8",
     IDENTITY_POOL_ID: "us-east-2:381f21a1-ca7b-4ab5-93e6-2f20666be2b2"
